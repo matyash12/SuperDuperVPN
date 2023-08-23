9702703d703a57ff16ddc04e8828a41a386be661
matyash12 <93146910+matyash12@users.noreply.github.com>
2023-08-23 11:17:35 +0200

    env file

--- mysite/mysite/settings_example.py
+++ mysite/mysite/settings_example.py
@@ -40,7 +40,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!dg=on5^7js79xrv$f)f!=97bf*i#%ruk(*1(rs!z--#4jo7w%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','django','nginx','10.0.0.*']
 
