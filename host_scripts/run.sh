fa6de51db335e13e6819cdb44153fe556b10220d
matyash12 <93146910+matyash12@users.noreply.github.com>
2023-08-28 16:05:24 +0200

    update

--- host_scripts/run.sh
+++ host_scripts/run.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# Define an array of script names
-script_array=("wireguard_logs.sh","apply_wireguard_conf.sh")
+# Get the directory where this script is located
+script_dir="$(dirname "$0")"
 
-# Loop through the array and execute each script
+# Define an array of script names
+script_array=("wireguard_logs.sh" "apply_wireguard_conf.sh")
+
+# Loop through the array and execute each script from the same directory
 for script in "${script_array[@]}"; do
-    if [ -f "$script" ]; then
+    full_script_path="$script_dir/$script"
+    if [ -f "$full_script_path" ]; then
         echo "Running $script..."
-        bash "$script"
+        bash "$full_script_path"
         echo "Finished running $script."
     else
-        echo "Script $script not found."
+        echo "Script $script not found in $script_dir."
     fi
 done
